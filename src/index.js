007254fe86631275cf9e4e6137d11dfca7692a88
SaumarDev <saumar_1@hotmail.com>
2022-07-25 11:49:39 -0500

    cambios al proyecto 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import validator from './validator.js';
 
-console.log(validator);
+let creditCardNumber = document.getElementById("numberCard")
+creditCardNumber.addEventListener("change", function(){
+if(validator.isValid(creditCardNumber.value)){
+    document.getElementById("isValidate").innerText = "La tarjeta es válida"
+    document.getElementById("isValidate").style.color = "green"
+}else{
+    document.getElementById("isValidate").innerText = "La tarjeta es inválida"
+    document.getElementById("isValidate").style.color = "red";
+}
+
+//Llamamos a maskify
+let maskifiedValue = validator.maskify(creditCardNumber.value)
+document.getElementById("numberCard").value = maskifiedValue
+});
+
+
+    
+
+    
